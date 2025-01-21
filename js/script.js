dd4345a9401cda69d9c5cd6523f3227777f2da4d
OmPimpale <ompimpale2003@gmail.com>
2025-01-22 03:41:53 +0530

    counterup added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    const counterUp = window.counterUp.default
+
+    const callback = entries => {
+        entries.forEach(entry => {
+            const el = entry.target
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    duration: 1500,
+                    delay: 16,
+                })
+                el.classList.add('is-visible')
+            }
+        })
+    }
+
+    const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+    const el = document.querySelectorAll('.counter');
+
+    for (let count = 0; count < el.length; count++) {
+        IO.observe(el[count])
+    }
+
+});
